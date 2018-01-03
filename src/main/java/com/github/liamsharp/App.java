51537746a463a5b638fb94db7e04df0829c7813e
Liam Sharp <liam.sharp@screamingfrog.co.uk>
2018-01-03 16:26:44 +0000

    Hellow World ui

--- src/main/java/com/github/liamsharp/App.java
+++ src/main/java/com/github/liamsharp/App.java
@@ -1,13 +1,35 @@
 package com.github.liamsharp;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application
 {
-    public static void main( String[] args )
+    
+    public static void main(String[] args) 
     {
-        System.out.println( "Hello World!" );
+        launch(args);
     }
+    
+    @Override
+    public void start(
+        final Stage primaryStage) 
+    {
+        primaryStage.setOnCloseRequest(event -> 
+        {
+            System.out.println("onCloseRequest called " + event);
+        });
+        
+        primaryStage.setTitle("Hello World!");
+        final Label label = new Label("Hello World!");
+        
+        final StackPane root = new StackPane();
+        root.getChildren().add(label);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+    
 }
